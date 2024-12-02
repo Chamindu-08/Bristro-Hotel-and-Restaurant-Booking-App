de1283837ad43a9501cac319cd34c110b69a2f4f
Manupriya253 <155411661+Manupriya253@users.noreply.github.com>
2024-12-03 00:43:31 +0530

    Done. AllRestaurantActivity.kt

--- app/src/main/java/com/nibm/bristrohotelandrestaurantbookingapp/AllRestaurantActivity.kt
+++ app/src/main/java/com/nibm/bristrohotelandrestaurantbookingapp/AllRestaurantActivity.kt
@@ -1,17 +1,19 @@
-package com.nibm.bristrohotelandrestaurantbookingapp
+package com.example.bristro_hotel_and_restaurant_booking_app
 
+
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class AllRestaurantActivity : AppCompatActivity() {
+open class Restaurant : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_all_restaurant)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.activity_all_restaurant) // Ensure the layout file name is correct
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.resturant_item)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
