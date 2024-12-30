4f0722797b7a715d023364be4754ac9e5aba4c3a
Manupriya253 <155411661+Manupriya253@users.noreply.github.com>
2024-12-30 22:16:41 +0530

    Done. activity_load.xml  and fix the error activity_all_restaurant.xml

--- app/src/main/java/com/nibm/bristrohotelandrestaurantbookingapp/AllRestaurantActivity.kt
+++ app/src/main/java/com/nibm/bristrohotelandrestaurantbookingapp/AllRestaurantActivity.kt
@@ -1,5 +1,4 @@
-package com.example.bristro_hotel_and_restaurant_booking_app
-
+package com.nibm.bristrohotelandrestaurantbookingapp
 
 import android.annotation.SuppressLint
 import android.os.Bundle
@@ -11,9 +10,10 @@ open class Restaurant : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_all_restaurant) // Ensure the layout file name is correct
+        setContentView(R.layout.activity_all_restaurant) // Ensure this layout exists
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.resturant_item)) { v, insets ->
+        // Handle system bar insets for the view
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.restaurant_item)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
